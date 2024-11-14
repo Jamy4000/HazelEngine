618720abc6c2f04e94eeb1060f88d0029cb79b97
Arnaud Briche <arnaud.briche@ubisoft.com>
2024-11-14 13:39:48 +0100

    Renderer2D now uses a single shader to render both textured and non-textured quads

--- HazelEngine/src/Platform/OpenGL/OpenGLTexture.cpp
+++ HazelEngine/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -3,10 +3,21 @@
 
 #include <stb_image.h>
 
-#include <glad/glad.h>
-
 namespace Hazel
 {
+    OpenGLTexture2D::OpenGLTexture2D(const uint32_t width, const uint32_t height)
+        : m_Width(width), m_Height(height), m_RendererID(-1), m_InternalFormat(GL_RGBA8), m_DataFormat(GL_RGBA)
+    {
+        glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+        glTextureStorage2D(m_RendererID, 1, m_InternalFormat, static_cast<int>(width), static_cast<int>(height));
+
+        glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    }
+
     OpenGLTexture2D::OpenGLTexture2D(const std::string& path) :
         m_Path(path), m_RendererID(-1)
     {
@@ -19,22 +30,23 @@ namespace Hazel
         m_Width = width;
         m_Height = height;
 
-        GLenum internalFormat = 0, dataFormat = 0;
+        m_InternalFormat = 0;
+        m_DataFormat = 0;
         if (channels == 4)
         {
-            internalFormat = GL_RGBA8;
-            dataFormat = GL_RGBA;
+            m_InternalFormat = GL_RGBA8;
+            m_DataFormat = GL_RGBA;
         }
         else if (channels == 3)
         {
-            internalFormat = GL_RGB8;
-            dataFormat = GL_RGB;
+            m_InternalFormat = GL_RGB8;
+            m_DataFormat = GL_RGB;
         }
 
-        HZ_CORE_ASSERT(internalFormat & dataFormat, "Format not supported!") 
+        HZ_CORE_ASSERT(m_InternalFormat & m_DataFormat, "Format not supported!") 
         
         glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
-        glTextureStorage2D(m_RendererID, 1, internalFormat, width, height);
+        glTextureStorage2D(m_RendererID, 1, m_InternalFormat, width, height);
 
         glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -42,7 +54,7 @@ namespace Hazel
         glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
         
-        glTextureSubImage2D(m_RendererID, 0, 0, 0, width, height, dataFormat, GL_UNSIGNED_BYTE, data);
+        glTextureSubImage2D(m_RendererID, 0, 0, 0, width, height, m_DataFormat, GL_UNSIGNED_BYTE, data);
 
         stbi_image_free(data);
     }
@@ -50,6 +62,15 @@ namespace Hazel
     OpenGLTexture2D::~OpenGLTexture2D()
     {
         glDeleteTextures(1, &m_RendererID);
+    }
+
+    void OpenGLTexture2D::SetData(void* data, uint32_t size)
+    {
+        const uint32_t bytesPerPixel = m_DataFormat == GL_RGBA ? 4 : 3;
+        HZ_CORE_ASSERT(size == m_Width * m_Height * bytesPerPixel, "Data must be entire Texture!")
+        
+        glTextureSubImage2D(m_RendererID, 0, 0, 0, static_cast<int>(m_Width), static_cast<int>(m_Height),
+            m_DataFormat, GL_UNSIGNED_BYTE, data);
     }
 
     void OpenGLTexture2D::Bind(const uint32_t slot) const
--- HazelEngine/src/Platform/OpenGL/OpenGLTexture.h
+++ HazelEngine/src/Platform/OpenGL/OpenGLTexture.h
@@ -2,16 +2,21 @@
 
 #include "Hazel/Renderer/Texture.h"
 
+#include <glad/glad.h>
+
 namespace Hazel
 {
     class OpenGLTexture2D final : public Texture2D
     {
     public:
+        OpenGLTexture2D(uint32_t width, uint32_t height);
         explicit OpenGLTexture2D(const std::string& path);
         ~OpenGLTexture2D() override;
         
         [[nodiscard]] uint32_t GetWidth() const override { return m_Width; }
         [[nodiscard]] uint32_t GetHeight() const override { return m_Height; }
+
+        void SetData(void* data, uint32_t size) override;
         
         void Bind(uint32_t slot) const override;
 
@@ -19,5 +24,6 @@ namespace Hazel
         std::string m_Path;
         uint32_t m_Width, m_Height;
         uint32_t m_RendererID;
+        GLenum m_InternalFormat, m_DataFormat;
     };
 }
