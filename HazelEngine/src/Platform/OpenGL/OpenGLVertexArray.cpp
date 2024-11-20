1d4105db36e582b3ecd16516f77de3e47e7fbeb6
Arnaud Briche <arnaud.briche@ubisoft.com>
2024-11-20 13:33:38 +0100

    Fixed incorrect cases in BufferElement::GetComponentCount

--- HazelEngine/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ HazelEngine/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -74,16 +74,52 @@ namespace Hazel
 		const auto& layout = buffer->GetLayout();
 		for (const auto& element : layout)
 		{
-			glEnableVertexAttribArray(m_VertexBufferIndex);
-			
-			glVertexAttribPointer(m_VertexBufferIndex,
-				static_cast<int>(element.GetComponentCount()),
-				ShaderDataTypeToOpenGLBaseType(element.Type),
-				element.Normalized ? GL_TRUE : GL_FALSE,
-				static_cast<int>(layout.GetStride()),
-				(const void*)element.Offset);
-			
-			m_VertexBufferIndex++;
+			switch (element.Type)
+			{
+				case ShaderDataType::Float:
+				case ShaderDataType::Float2:
+				case ShaderDataType::Float3:
+				case ShaderDataType::Float4:
+				case ShaderDataType::Int:
+				case ShaderDataType::Int2:
+				case ShaderDataType::Int3:
+				case ShaderDataType::Int4:
+				case ShaderDataType::Bool:
+				{
+					glEnableVertexAttribArray(m_VertexBufferIndex);
+					glVertexAttribPointer(m_VertexBufferIndex,
+						static_cast<int>(element.GetComponentCount()),
+						ShaderDataTypeToOpenGLBaseType(element.Type),
+						element.Normalized ? GL_TRUE : GL_FALSE,
+						static_cast<int>(layout.GetStride()),
+						(const void*)element.Offset);
+					m_VertexBufferIndex++;
+					break;
+				}
+				
+				case ShaderDataType::Mat3:
+				case ShaderDataType::Mat4:
+				{
+					const uint32_t count = element.GetComponentCount();
+					for (uint32_t i = 0; i < count; i++)
+					{
+						glEnableVertexAttribArray(m_VertexBufferIndex);
+						glVertexAttribPointer(m_VertexBufferIndex,
+							static_cast<int>(count),
+							ShaderDataTypeToOpenGLBaseType(element.Type),
+							element.Normalized ? GL_TRUE : GL_FALSE,
+							static_cast<int>(layout.GetStride()),
+							(const void*)(sizeof(float) * count * i));
+						glVertexAttribDivisor(m_VertexBufferIndex, 1);
+						m_VertexBufferIndex++;
+					}
+					break;
+				}
+				
+				case ShaderDataType::None:
+				default:
+					HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+			}
 		}
 
 		m_VertexBuffers.push_back(buffer);
